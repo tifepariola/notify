51fb0dc466a29e6fb9a40d30445bdbb42b2b160e
TiFe Pariola <tife@pario.la>
2025-03-20 23:41:20 +0100

    fix

--- wa.js
+++ wa.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
